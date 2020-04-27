0ff9bbcfb9b051f835965df38e6c47f50f14316b
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-27 00:00:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,19 +7,19 @@ def starts_with_a_vowel?(word)
    end
  end
 
- 
+
   def words_starting_with_un_and_ending_with_ing(text)
 
       text.scan(/un+\w+ing\b/)
  end
 
- 
+
   def words_five_letters_long(text)
 
     text.scan(/\b\w{5}\b/)
- end	
+ end
 
- 
+
   def first_word_capitalized_and_ends_with_punctuation?(text)
 
     if text.match(/^[A-Z].+[\.!?]$/) then
@@ -27,9 +27,9 @@ def starts_with_a_vowel?(word)
    else
      return false
    end
- end		 
+ end
 
- 
+
    def valid_phone_number?(phone)
 
     if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) then
