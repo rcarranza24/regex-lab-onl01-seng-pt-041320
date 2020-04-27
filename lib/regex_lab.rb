dd9d66fc190d67fcc0b7385f6d60971c96beb028
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-27 00:00:27 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,40 @@
 def starts_with_a_vowel?(word)
 
-end
+    if word.match(/^[aeiouAEIOU]+\w/) then
+     return true
+   else
+     return false
+   end
+ end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
-end
+  def words_starting_with_un_and_ending_with_ing(text)
 
-def words_five_letters_long(text)
+      text.scan(/un+\w+ing\b/)
+ end
 
-end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
+  def words_five_letters_long(text)
 
-end
+    text.scan(/\b\w{5}\b/)
+ end
 
-def valid_phone_number?(phone)
 
-end
+  def first_word_capitalized_and_ends_with_punctuation?(text)
+
+    if text.match(/^[A-Z].+[\.!?]$/) then
+     return true
+   else
+     return false
+   end
+ end
+
+
+   def valid_phone_number?(phone)
+
+    if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) then
+     return true
+   else
+     return false
+   end
+ end		 
